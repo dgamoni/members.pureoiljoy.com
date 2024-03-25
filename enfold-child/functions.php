59c2c91f9a6a4481bae8733dd438114a90332612
dgamoni <dgamoni@gmail.com>
2024-03-25 18:17:40 +0100

    first commit

---
+++ enfold-child/functions.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+* Add your own functions here. You can also copy some of the theme functions into this file. 
+* Wordpress will use those functions instead of the original functions then.
+*/
+
+
+//Disable html in wordpress comments
+
+add_filter( ‘pre_comment_content’, ‘wp_specialchars’);
+
+//Remove Query Strings from Static Resources
+
+function _remove_script_version( $src ){
+	$parts = explode( '?ver', $src );
+	return $parts[0];
+}
+add_filter( 'script_loader_src', '_remove_script_version', 15, 1 );
+add_filter( 'style_loader_src', '_remove_script_version', 15, 1 );
+
+/** Disable the emoji's */
+
+function disable_emojis() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );	
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );	
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+}
+add_action( 'init', 'disable_emojis' );
+
+/**
+ * Filter function used to remove the tinymce emoji plugin.
+ * 
+ * @param    array  $plugins  
+ * @return   array             Difference betwen the two arrays
+ */
+function disable_emojis_tinymce( $plugins ) {
+	if ( is_array( $plugins ) ) {
+		return array_diff( $plugins, array( 'wpemoji' ) );
+	} else {
+		return array();
+	}
+}
+
+// Remove WP embed script
+
+function speed_stop_loading_wp_embed() {
+	if (!is_admin()) {
+		wp_deregister_script('wp-embed');
+	}
+}
+add_action('init', 'speed_stop_loading_wp_embed');
+
+
+/* to support displaying custom post types */
+
+add_theme_support(‘add_avia_builder_post_type_option’);
+add_theme_support(‘avia_template_builder_custom_post_type_grid’);
+
